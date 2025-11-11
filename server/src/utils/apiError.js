e20b5b09f0224d5e309e7a862faef6f7727512b1
TheWallOnFire <tuongmanhhuynh2005physic@gmail.com>
2025-11-11 23:39:23 +0700

    Initial commit: Set up monorepo structure

--- server/src/utils/apiError.js
+++
@@ -1,9 +0,0 @@
-// A custom Error class for our API
-class ApiError extends Error {
-    constructor(statusCode, message) {
-        super(message);
-        this.statusCode = statusCode;
-    }
-}
-
-module.exports = ApiError;
--- server/src/utils/validators.js
+++
@@ -1,7 +0,0 @@
-const isValidEmail = (email) => {
-  if (typeof email !== 'string') return false;
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email.toLowerCase());
-};
-
-module.exports = { isValidEmail };
